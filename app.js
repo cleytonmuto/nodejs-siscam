bc0604834ff10c611d60c7c8745d7788b48af76e
cleytonmuto <cleytonmuto@gmail.com>
2022-09-06 10:56:35 -0300

    init: commit inicial da aplicação

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+
+app.get('/', function(req,res) {
+  res.send('Hello, world!');
+});
+
+app.listen(3000, function() {
+  console.log('Server running on port 3000...');
+});
+
