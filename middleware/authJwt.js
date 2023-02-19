464b8c8d5b6da81cc09b4d5b83ff3005303489bb
cleytonmuto <cleytonmuto@gmail.com>
2023-02-19 07:00:50 -0300

    refactor: middleware

--- middleware/authJwt.js
+++ middleware/authJwt.js
@@ -6,38 +6,38 @@ const db = require('../models');
 const Usuario = db.usuario;
 
 const verifyToken = (req, res, next) => {
-    const token = req.headers.authorization.split(' ')[1]
-    if (!token) {
-        return res.status(403).send({
-            message: 'Nenhum token fornecido.'
-        });
-    }
-    jwt.verify(token, config.secret, (err, decoded) => {
-        if (err) {
-            return res.status(401).send({
-                message: 'Não autorizado.'
-            });
-        }    
-        req.userId = decoded.id;
-        next();
+  const token = req.headers.authorization.split(' ')[1]
+  if (!token) {
+    return res.status(403).send({
+      message: 'Nenhum token fornecido.'
     });
+  }
+  jwt.verify(token, config.secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({
+        message: 'Não autorizado.'
+      });
+    }
+    req.userId = decoded.id;
+    next();
+  });
 };
 
 const isAdmin = (req, res, next) => {
-    Usuario.findByPk(req.userId).then((user) => {
-        user.getRoles().then((roles) => {
-            for (let i = 0; i < roles.length; i++) {
-                if (roles[i].name === 'admin') {
-                    next();
-                    return;
-                }
-            }
-            res.status(403).send({
-                message: 'Requer perfil de administrador.'
-            });
-            return;
-        });
+  Usuario.findByPk(req.userId).then((user) => {
+    user.getRoles().then((roles) => {
+      for (let i = 0; i < roles.length; i++) {
+        if (roles[i].name === 'admin') {
+          next();
+          return;
+        }
+      }
+      res.status(403).send({
+        message: 'Requer perfil de administrador.'
+      });
+      return;
     });
+  });
 };
 
 module.exports = { verifyToken, isAdmin };
--- middleware/verifySignUp.js
+++ middleware/verifySignUp.js
@@ -5,61 +5,61 @@ const ROLES = db.ROLES;
 const Usuario = db.usuario;
 
 const checkDuplicates = (req, res, next) => {
-    // Email
-    Usuario.findOne({
-        where: {
-            email: req.body.email
-        }
-    })
+  // Email
+  Usuario.findOne({
+    where: {
+      email: req.body.email
+    }
+  })
     .then((user) => {
-        if (user) {
+      if (user) {
+        res.status(400).send({
+          message: 'Erro. Email já cadastrado.'
+        });
+        return;
+      }
+      // CPF
+      Usuario.findOne({
+        where: {
+          cpf: req.body.cpf
+        }
+      })
+        .then((user) => {
+          if (user) {
             res.status(400).send({
-                message: 'Erro. Email já cadastrado.'
+              message: 'Erro. CPF já cadastrado.'
             });
             return;
-        }
-        // CPF
-        Usuario.findOne({
+          }
+          // Name
+          Usuario.findOne({
             where: {
-                cpf: req.body.cpf
+              name: req.body.name
             }
-        })
-        .then((user) => {
-            if (user) {
-                res.status(400).send({
-                    message: 'Erro. CPF já cadastrado.'
-                });
-                return;
-            }
-            // Name
-            Usuario.findOne({
-                where: {
-                    name: req.body.name
-                }
-            })
+          })
             .then((user) => {
-                if (user) {
-                    res.status(400).send({ message: 'Erro. Nome já cadastrado.' });
-                    return;
-                }
-                next();
+              if (user) {
+                res.status(400).send({ message: 'Erro. Nome já cadastrado.' });
+                return;
+              }
+              next();
             });
         });
     });
 };
 
 const checkRoles = (req, res, next) => {
-    if (req.body.roles) {
-        for (let i = 0; i < req.body.roles.length; i++) {
-            if (!ROLES.includes(req.body.roles[i])) {
-                res.status(400).send({
-                    message: `Erro. Perfil inexistente = ${req.body.roles[i]}`
-                });
-                return;
-            }
-        }
-    }
-    next();
+  if (req.body.roles) {
+    req.body.roles.forEach((role) => {
+      if (!ROLES.includes(role)) {
+        res.status(400).send({
+          message: `Erro. Perfil inexistente = ${role}`
+        });
+        return;
+      }
+    });
+  }
+  next();
 };
 
 module.exports = { checkDuplicates, checkRoles };
